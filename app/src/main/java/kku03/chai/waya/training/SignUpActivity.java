8a1ad3b5abb1a88fa2b01d768c54f3227a8bc7c6
Wayamon <m.wayama@kkumail.com>
2016-11-12 12:04:49 +0700

    +SignUP Button

---
+++ app/src/main/java/kku03/chai/waya/training/SignUpActivity.java
@@ -0,0 +1,13 @@
+package kku03.chai.waya.training;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
